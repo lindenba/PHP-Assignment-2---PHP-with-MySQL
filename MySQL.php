bf9e8c5112d0c44cd7edd7aed1f0381c5488f944
Anne Lindenberg <lindenba@onid.orst.edu>
2015-05-06 16:15:16 -0700

    Trying to figure out how to make my table. Not understanding from the lectures what exactly I am doing.  Looking at videos on php and mysql, but there is a program that I am suppose to download.  Wondering do I have to download this program.  Really bothers me that the assignments can't reflect what the weekly videos are about.

--- MySQL.php
+++ MySQL.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html>
+<header>
+  <meta charset="UTF-8">
+  <title>Video Inventory</title>
+</header>
+<body>
+  <form action="MySQL.php" method="POST">
+    Name: <input type="text" name="video_name">
+    Category: <input type="text" name="Type_video">
+    Length: <input type="number" name="Length_video" min=0>
+    <input type="submit" value="Submit">
+  </form>
+</body>
+</html>
 <?php
 ini_set('display_erros', 'On');
 
@@ -11,3 +26,9 @@ else
   echo "Connection worked!";
 }
 ?>
+<?php
+
+// mysql> CREATE TABLE videoInventory  (id INT PRIMARY KEY AUTO_INCREMENT,
+//   name VARCHAR(255) UNIQUE NOT NULL,
+//   category VARCHAR(255),
+//   length INT, rented INT NOT NULL DEFAULT 0 );
