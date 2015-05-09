aa950f835218fe95ca40e05cc193b3e59c72c6dd
Anne Lindenberg <lindenba@onid.orst.edu>
2015-05-09 12:35:47 -0700

    Have my adding a video and fetching video inventory from mysqli database table

--- vid_inventory.php
+++ vid_inventory.php
@@ -5,19 +5,19 @@ ini_set('display_erros', 'On');
 
 <!DOCTYPE html>
 <html>
-<header>
-  <meta charset="UTF-8">
-  <title>Video Inventory</title>
-</header>
+  <header>
+    <meta charset="UTF-8">
+    <title>Video Inventory</title>
+  </header>
 <body>
-  <form action="vid_inventory.php" method="POST">
-    <h3>Add a Video:</h3>
-    Name: <input type="text" name="name">
-    Category: <input type="text" name="category">
-    Length: <input type="number" name="length">
-    <input type="submit" name="addVideo" value="Add">
-  </form>
-  <?php
+    <form action="vid_inventory.php" method="POST">
+      <h2>Add a Video:</h2>
+        Name: <input type="text" name="name">
+        Category: <input type="text" name="category">
+        Length: <input type="number" name="length">
+        <input type="submit" name="addVideo" value="Add">
+    </form>
+<?php
   $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "lindenba-db", "ntVB3yI2RNUm7xHg", "lindenba-db");
   if($mysqli->connect_errno)
   {
@@ -57,7 +57,7 @@ if($_POST){
 
       if($required)
       {
-        echo "Could not add video, please enter into all catagories";
+        echo "Could not add video, please enter into all categories";
       }
       else{
 
@@ -77,6 +77,40 @@ if($_POST){
     }
   }
 ?>
+
+<h2>Video Inventory List</h2>
+  <table>
+    <tr>
+        <td>Name</td>
+        <td>Category</td>
+        <td>Length</td>
+        <td>Available</td>
+    </tr>
+<?php
+    //select from table get name, cat, len, and rented
+    if(!($stmt = $mysqli->prepare("SELECT name, category, length, rented FROM videoInventory")))
+    {
+      echo "Prepare failed: " .$mysqli->connect_errno. " " .$mysqli->connect_error;
+    }
+    if(!$stmt->execute())
+    {
+      echo "Execute failed: " .$mysqli->connect_errno. " " .$mysqli->connect_error;
+    }
+    //bind those results into these variables
+    if(!$stmt->bind_result($name, $category, $length, $rented))
+    {
+      echo "Bind failed: " .$mysqli->connect_errno. " " .$mysqli->connect_error;
+    }
+    // //this will fetch until it can't fetch any more
+    while($stmt->fetch())
+    {
+      echo "<tr>\n<td>\n" . $name . "\n</td>\n<td>\n" . $category .
+           "\n</td>\n<td>\n" . $length . "\n</td>\n<td>\n". $rented . "\n</td>\n</tr>";
+    }
+
+?>
+
+  </table>
 
 
 </body>
